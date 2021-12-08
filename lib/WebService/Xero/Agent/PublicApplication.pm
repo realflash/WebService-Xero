ec08ee2bf5e6bc6c8c03d89efb8d237b32f5f846
Ian Gibbs <igibbs@cpan.org>
2021-12-08 10:34:44 +0000

    Switch OAuth library to Net::OAuth2

--- lib/WebService/Xero/Agent/PublicApplication.pm
+++ lib/WebService/Xero/Agent/PublicApplication.pm
@@ -11,7 +11,7 @@ use Digest::MD5 qw( md5_base64 );
 
 use URI::Encode qw(uri_encode uri_decode );
 use Data::Random qw( rand_chars );
-use Net::OAuth 0.20;
+use Net::OAuth2 0.67;
 $Net::OAuth::PROTOCOL_VERSION = Net::OAuth::PROTOCOL_VERSION_1_0A;
 
 =head1 NAME
